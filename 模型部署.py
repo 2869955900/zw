dcb91961f97aa833d11202898d830b74a969292a
2869955900 <100764356+2869955900@users.noreply.github.com>
2025-03-26 12:23:20 +0800

    Update 模型部署.py

--- 模型部署.py
+++ 模型部署.py
@@ -15,9 +15,9 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-# 设置matplotlib支持中文和负号，这里以微软雅黑为例
-plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']
-plt.rcParams['axes.unicode_minus'] = False    # 正常显示负号
+# 设置matplotlib支持中文和负号
+plt.rcParams['font.sans-serif'] = 'SimHei'
+plt.rcParams['axes.unicode_minus'] = False
 
 # 加载模型
 model_path = "RandomForestRegressor.pkl"
